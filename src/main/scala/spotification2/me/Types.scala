db66a9ee515f61cbb0c133d1d7cb6ec0cf73fa5f
Marcelo Gomes <marcelo.gomes@commercetools.com>
2023-08-11 18:05:42 +0200

    Port me types

---
+++ src/main/scala/spotification2/me/Types.scala
@@ -0,0 +1,7 @@
+package spotification2.me
+
+import spotification2.common.Opaque
+import spotification2.common.UriString
+
+object MeApiUri extends Opaque[UriString]
+type MeApiUri = MeApiUri.OpaqueType

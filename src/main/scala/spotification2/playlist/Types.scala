6db036738a882d8128b54e63a4451c0f7cd47cff
Marcelo Gomes <marcelo.gomes@commercetools.com>
2023-08-25 14:48:31 +0200

    Port playlist types

---
+++ src/main/scala/spotification2/playlist/Types.scala
@@ -0,0 +1,23 @@
+package spotification2.playlist
+
+import eu.timepit.refined.collection.MinSize
+import eu.timepit.refined.collection.MaxSize
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.boolean.And
+import spotification2.common.Opaque
+import spotification2.common.SpotifyId
+import spotification2.common.UriString
+
+// A maximum of 100 Tracks can be processed in a single request
+// An IndexedSeq is being used due to efficient `length` operation (needed for the refinement)
+type PlaylistItemsToProcessMax = 100
+type PlaylistItemsToProcessP = MinSize[1] And MaxSize[PlaylistItemsToProcessMax]
+type PlaylistItemsToProcess[A] = Vector[A] Refined PlaylistItemsToProcessP
+object PlaylistItemsToProcess:
+  val MaxSize: PlaylistItemsToProcessMax = valueOf[PlaylistItemsToProcessMax]
+
+object PlaylistId extends Opaque[SpotifyId]
+type PlaylistId = PlaylistId.OpaqueType
+
+object PlaylistApiUri extends Opaque[UriString]
+type PlaylistApiUri = PlaylistApiUri.OpaqueType

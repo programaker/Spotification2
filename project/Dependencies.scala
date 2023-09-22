1b7174f4cf0819ac1ee005e3b706eabe39354838
Marcelo Gomes <marcelo.gomes@commercetools.com>
2023-09-22 15:26:44 +0200

    Update libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,26 +26,25 @@ object Dependencies {
     "com.disneystreaming" %% "weaver-cats" % Versions.Weaver % Test,
     "com.disneystreaming" %% "weaver-scalacheck" % Versions.Weaver % Test,
 
-    "com.github.valskalla" %% "odin-core" % Versions.OdinCore,
-
     "com.github.pureconfig" %% "pureconfig-core" % Versions.PureConfigCore,
 
     "co.fs2" %% "fs2-core" % Versions.Fs2Core,
 
+    "org.typelevel" %% "log4cats-slf4j" % Versions.Log4Cats,
     "ch.qos.logback" % "logback-classic" % Versions.Logback,
   )
 
   private object Versions {
     val Refined = "0.11.0"
-    val CatsCore = "2.9.0"
+    val CatsCore = "2.10.0"
     val CatsEffect = "3.5.1"
-    val Fs2Core = "3.7.0"
-    val CirceCore = "0.14.5"
-    val OdinCore = "0.13.0"
+    val Fs2Core = "3.9.2"
+    val CirceCore = "0.14.6"
     val PureConfigCore = "0.17.4"
-    val Logback = "1.4.8"
+    val Log4Cats = "2.6.0"
+    val Logback = "1.4.11"
     val Mouse = "1.2.1"
-    val TapirCore = "1.6.3"
+    val TapirCore = "1.7.4"
     val Http4s = "0.23.15"
     val Weaver = "0.8.3"
   }

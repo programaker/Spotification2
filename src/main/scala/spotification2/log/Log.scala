e89cb8d598c513fa7cb313030d85b0bde4f49437
Marcelo Gomes <marcelo.gomes@commercetools.com>
2023-09-22 18:52:11 +0200

    Making log4cats more palatable

--- src/main/scala/spotification2/log/Log.scala
+++ src/main/scala/spotification2/log/Log.scala
@@ -1,9 +1,9 @@
 package spotification2.log
 
-import org.typelevel.log4cats.Logger
 import cats.effect.IO
-import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.LoggerFactory
+import org.typelevel.log4cats.slf4j.Slf4jFactory
 
-type Log = Logger[IO]
+type Log = LoggerFactory[IO]
 object Log:
-  def make: IO[Log] = Slf4jLogger.create[IO]
+  given Log = Slf4jFactory.create

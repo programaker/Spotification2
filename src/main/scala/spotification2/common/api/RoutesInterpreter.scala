ce201e7d5a719916a194cee4c743217ef7b72448
Marcelo Gomes <marcelo.gomes@commercetools.com>
2023-11-23 11:33:14 +0100

    Idea: change the http server/framework

--- src/main/scala/spotification2/common/api/RoutesInterpreter.scala
+++ src/main/scala/spotification2/common/api/RoutesInterpreter.scala
@@ -3,6 +3,12 @@ package spotification2.common.api
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 import cats.effect.IO
 
+// Http4s kinda sucks regarding performance.
+// As Tapir abstracts the http server/framework, lets find something better!
+// Search service is using NettyCats. Vertx seems very good too.
+// https://www.techempower.com/benchmarks/#hw=ph&test=fortune&section=data-r22
+// https://tapir.softwaremill.com/en/latest/
+
 type RoutesInterpreter = Http4sServerInterpreter[IO]
 object RoutesInterpreter:
   given RoutesInterpreter = Http4sServerInterpreter()
